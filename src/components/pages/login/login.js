1e993584ec32b09c71b9154915987ac239283f4e
janouser11 <janouser11@gmail.com>
2018-10-02 21:31:02 -0400

    Added some basic styling to login page and added google analytics

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -1,10 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
+import moose from "./moose.svg";
 import "./login.css";
 
 const callBackUrl = process.env.REACT_APP_CALLBACK_URL;
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const response_type = process.env.REACT_APP_RESPONSE_TYPE;
+const login_url = `https://accounts.spotify.com/authorize/?client_id=${client_id}&response_type=${response_type}&redirect_uri=${callBackUrl}&scope=user-read-private%20user-read-email&state=34fFs29kd09`;
 
 class Home extends React.Component {
   render() {
@@ -12,18 +13,23 @@ class Home extends React.Component {
     console.log(client_id);
     console.log(response_type);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the beginning of Mousai</h1>
-        </header>
-        <p className="App-intro">Currently in development</p>
-        <p>Coming soon...</p>
-        <a
-          href={`https://accounts.spotify.com/authorize/?client_id=${client_id}&response_type=${response_type}&redirect_uri=${callBackUrl}&scope=user-read-private%20user-read-email&state=34fFs29kd09`}
-        >
-          Spotify Sign In
-        </a>
+      <div className="login-page">
+        <div>
+          <div className="logo-container">
+            <div className="circle-outline">
+              <img src={moose} className="logo" alt="logo" />
+            </div>
+          </div>
+
+          <h1 className="title">Welcome to Mousai</h1>
+          <p className="sub-title">Login to your Spotify account to continue</p>
+
+          <div className="login-button-container">
+            <a className="login-button" href={login_url}>
+              Login
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
